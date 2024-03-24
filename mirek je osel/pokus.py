a632e31bceb1096bd5e5eda501dd3ffc3dc5c38e
tomas janda <tomas.janda885@gmail.com>
2024-03-24 15:42:55 +0100

    sound effect mirek je osel(donkey)

---
+++ mirek je osel/pokus.py
@@ -0,0 +1,39 @@
+
+import playsound
+import sys # to access the system
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#print("Tata je žába")
+#playsound.playsound("C:/Users/tomas/Downloads/zaba.mp3")
+#playsound.playsound("C:/Users/tomas/Downloads/osel.mp3")
+
+#img = mpimg.imread('your_image.png')
+#imgplot = plt.imshow(img)
+#plt.show()
+
+
+for i in range (1,1000000000000000000000):
+    print("Míra je osel")
+    playsound.playsound("C:/Users/tomas/Downloads/Recording.mp3")
+    playsound.playsound("C:/Users/tomas/Downloads/osel2.mp3")
+    print ("čislo je " , i, " osel" , i+1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
