4d1893849d28b55a1398a486839abb7a0a1d3b7b
tomas janda <tomas.janda885@gmail.com>
2024-03-24 16:03:41 +0100

    nova cesta ke zvukum

--- mirek je osel/pokus.py
+++ mirek je osel/pokus.py
@@ -3,7 +3,7 @@ import playsound
 import sys # to access the system
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+import os
 #print("Tata je žába")
 #playsound.playsound("C:/Users/tomas/Downloads/zaba.mp3")
 #playsound.playsound("C:/Users/tomas/Downloads/osel.mp3")
@@ -12,11 +12,13 @@ import matplotlib.image as mpimg
 #imgplot = plt.imshow(img)
 #plt.show()
 
+cesta = os.getcwd()
+
 
 for i in range (1,1000000000000000000000):
     print("Míra je osel")
-    playsound.playsound("C:/Users/tomas/Downloads/Recording.mp3")
-    playsound.playsound("C:/Users/tomas/Downloads/osel2.mp3")
+    playsound.playsound(cesta + "/Recording.mp3")
+    playsound.playsound(cesta + "/osel2.mp3")
     print ("čislo je " , i, " osel" , i+1)
 
 
